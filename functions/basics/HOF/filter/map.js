8ba78655e6cf0c1876d9cbe50cc2933d9288ea5f
Krushna <krish762090@gmail.com>
2026-02-22 23:08:25 +0530

    learned map

---
+++ functions/basics/HOF/filter/map.js
@@ -0,0 +1,5 @@
+const mynum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const myNumber = mynum.map((number) => number * 10 ).map((number) => number + 1).filter((number)=> number >= 40)
+
+console.log(myNumber);
