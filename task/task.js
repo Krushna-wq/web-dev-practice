16dc2b63813ec0f5d6474ddeed0f7861cdcdfd64
Krushna <krish762090@gmail.com>
2026-03-19 02:58:55 +0530

    learning this and solving task

---
+++ task/task.js
@@ -0,0 +1,55 @@
+const cart = [
+ {name:"Shirt", price:500, qty:2},
+ {name:"Jeans", price:1000, qty:1},
+ {name:"Shoes", price:2000, qty:1}
+];
+
+const total = cart.reduce((acc, item) => {
+    return acc + item.price;
+}, 0)
+
+console.log(total)
+
+
+const ProductName = cart.forEach((item) => console.log(`Product name is ${item.name}`))
+
+const Filtered = cart.filter((item) => {
+    return item.price > 1000;
+})
+
+console.log(Filtered);
+
+
+const qty = cart.filter((item) => {
+    return item.qty > 1
+})
+
+console.log(qty)
+
+const totalQty = cart.reduce((acc, item) => {
+    return acc + item.qty; 
+}, 0)
+
+console.log(totalQty)   
+
+const check = cart.reduce((acc, item) => {
+    return  item.instock = true;
+}, true)
+
+console.log(check)
+
+console.log(cart)
+
+const upper = cart.map((item) => item.name.toUpperCase())
+
+console.log(upper)
+
+const highItem = cart.forEach((item) => {
+    let high = 0;
+    if(high<item.price){
+        high = item.price;
+    }
+    return high;
+})
+
+console.log(highItem);
