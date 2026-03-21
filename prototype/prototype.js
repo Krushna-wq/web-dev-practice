a618b3e35ee0ed24c039e6d9bbc0f508c78ad7b2
Krushna <krish762090@gmail.com>
2026-03-22 04:09:33 +0530

    building HOF using prototype

--- prototype/prototype.js
+++ prototype/prototype.js
@@ -1,13 +1,32 @@
-Array.prototype.myMap = function(callback) {
-  let result = [];
-
-  for (let i = 0; i < this.length; i++) {
-    // skip empty indexes (important edge case)
-    if (i in this) {
-      result.push(callback(this[i], i, this));
+Array.prototype.mapTwo = function(hello) {
+    let result = [];
+    for(let i = 0; i<this.length; i++){
+        result.push(hello(this[i], i, this));
     }
-  }
-
-  return result;
+     return result;
 };
+
+
+function hello(value){
+   return value + 2;
+}
+
+console.log([1, 2, 3, 4].mapTwo(hello));
+
+
+
+Array.prototype.Myreduce = function(cb, initialvalue){
+    let acc = initialvalue;
+    for(let i = 0; i<this.length ; i++){
+        acc = cb(acc, this[i], i, this)
+    }
+    return acc;
+}
+
+function calculate(acc, curr){
+   return acc + curr;
+}
+
+console.log([1, 2, 3].Myreduce(calculate, 0))
+
 
