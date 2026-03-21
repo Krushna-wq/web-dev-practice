f9a9de1f0090a82d74a58084295601d22c07733a
Krushna <krish762090@gmail.com>
2026-03-22 03:23:11 +0530

    building HOF using prototype

--- new/practice.js
+++ new/practice.js
@@ -65,3 +65,19 @@ function hello(value){
 }
 
 console.log([1, 2, 3, 4].mapTwo(hello));
+
+
+
+Array.prototype.Myreduce = function(cb, initialvalue){
+    let acc = initialvalue;
+    for(let i = 0; i<this.length ; i++){
+        acc = cb(acc, this[i], i, this)
+    }
+    return acc;
+}
+
+function calculate(acc, curr){
+   return acc + curr;
+}
+
+console.log([1, 2, 3].Myreduce(calculate, 0))
